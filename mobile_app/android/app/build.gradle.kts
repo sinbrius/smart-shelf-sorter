c6c6abd66d4f58e0b823de289a1e3defe9e419fd
sinbrius <sinbrius@gmail.com>
2026-08-19 15:42:03 +0300

    demo eklendi

--- mobile_app/android/app/build.gradle.kts
+++ mobile_app/android/app/build.gradle.kts
@@ -13,6 +13,7 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+   
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
